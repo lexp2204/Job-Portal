7b7b11748131cf5436298fefc7018ee9f21c1bb1
Alexis Penn <lexp.0304@gmail.com>
2025-04-08 17:19:59 -0400

    Landing Page is done, I added Navigation

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import Navbar from "../components/Navbar";
-import {Link} from "react-router-dom"
+import "../Landing.css"
+import jobSearchimg from "../assets/job-search.svg"
+
 
 function Landing(){
+    const navigate = useNavigate();
     return(
         <>
         <Navbar />
-        <h1>Landing Page</h1>
+        <div className="container">
+            <div className="hero-txt-container">
+                <p className="hero-txt">Use <span className="job-finder-txt">JobFinder</span>, For<br /> Making Finding a Job<br /> Simple. </p>
+                <p className="subheadline">JobFinder is here to simplify your job search and point you to your dream job.</p> 
+            </div>
+            <div className="img-container">
+                <img src={jobSearchimg}></img>
+            </div> 
+        </div>
+        <div className="btn-container">
+            <button className="login" onClick={()=>navigate("/login")}>Login</button>
+            <button className="signup" onClick={()=>navigate("/register")}>Signup</button>
+        </div>
         </>
     )
 }
