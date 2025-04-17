f3f2288d0981d64194b93ec750a867f1f522a537
Alexis Penn <lexp.0304@gmail.com>
2025-04-17 19:27:36 -0400

    Starting Login Page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Login(){
     return(
         <>
-        
+            <div className="nav-container">
+                <div className="logo-container">
+                    <p className="logo"><Link to="/">JobFinder</Link></p>
+                </div>
+                <div className="nav-btn-container">
+                    <ul>
+                        <li className="signup-btn"><Link to="../register">Signup</Link></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="login-card">
+                <p>Welcome Back!</p>
+                <div className="img-container">
+
+                </div>
+            </div>
+
+
         </>
     )
 }
